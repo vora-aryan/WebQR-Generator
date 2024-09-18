a114863d804d407a802de938732184d6b020d4c3
Aryan <aryanvora619@gmail.com>
2024-09-18 18:54:34 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const QRCode = require("qrcode");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+function isValidUrl(url) {
+  const regex = /^(http:\/\/|https:\/\/|www\.)/i;
+  return regex.test(url);
+}
+
+app.get("/", (req, res) => {
+  res.render("index", { error: null });
+});
+
+app.post("/generate", (req, res) => {
+  const url = req.body.url;
+
+  if (!isValidUrl(url)) {
+    return res.render("index", {
+      error: "Please enter a valid URL.",
+    });
+  }
+
+  QRCode.toDataURL(url, (err, src) => {
+    if (err) res.send("Error occured.!");
+    res.render("generate", { src, url });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server started on http://localhost:3000");
+});
